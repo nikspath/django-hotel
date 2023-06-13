dafa252cc8e1015432b9fa5dfb02a4e6c9f9495a
nikhil <dropboxnikspath@gmail.com>
2023-06-13 20:52:17 +0530

    register

--- customeadmin/urls.py
+++ customeadmin/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('admin-login/', login_admin, name='login_admin'),
     path('admin-logout/', logout_admin, name='logout-admin'),
+    path('admin-register/', admin_register, name='register-admin'),
 ]
--- customeadmin/views.py
+++ customeadmin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -24,3 +24,22 @@ def login_admin(request):
 def logout_admin(request):
 	logout(request)
 	return render(request,'login.html') 	
+
+
+def admin_register(request):
+	print("ssssss",request.POST.get('password'))
+	if request.method == 'POST':
+		username=request.POST.get('username')
+		password=request.POST.get('password')
+		user_obj = User.objects.filter(username=username)
+		if not user_obj.exists():
+			user=User.objects.create(username=username,is_staff=True,is_active=True,is_superuser=True)
+			user.set_password(password)	
+			user.save()
+			messages.success(request,"user saved")
+			return redirect("/tpl/admin-login/")
+		else:
+			messages.warning(request,"already exist")
+
+
+	return render(request,'register.html')	
